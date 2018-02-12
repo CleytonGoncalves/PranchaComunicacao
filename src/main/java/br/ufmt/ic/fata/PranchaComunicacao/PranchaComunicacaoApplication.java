794d56570cd3aaf0aebef0e12c2ee9218757ffa4
CleytonGoncalves <cleyton.cgoncalves@gmail.com>
2018-02-12 15:45:50 -0300

    Adiciona palavras de teste aos pacientes de teste

--- src/main/java/br/ufmt/ic/fata/PranchaComunicacao/PranchaComunicacaoApplication.java
+++ src/main/java/br/ufmt/ic/fata/PranchaComunicacao/PranchaComunicacaoApplication.java
@@ -6,7 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 
+import java.util.HashSet;
+
+import br.ufmt.ic.fata.PranchaComunicacao.modelo.Paciente;
 import br.ufmt.ic.fata.PranchaComunicacao.servico.armazenamento.ArmazenamentoDiscoServico;
+import br.ufmt.ic.fata.PranchaComunicacao.servico.paciente.PacienteServico;
+import br.ufmt.ic.fata.PranchaComunicacao.servico.palavra.ComplementoServico;
+import br.ufmt.ic.fata.PranchaComunicacao.servico.palavra.DiversoServico;
+import br.ufmt.ic.fata.PranchaComunicacao.servico.palavra.SujeitoServico;
+import br.ufmt.ic.fata.PranchaComunicacao.servico.palavra.VerboServico;
 
 @SpringBootApplication
 @PropertySource(value = {"application.properties", "segredos.application.properties"})
@@ -20,8 +28,21 @@ public class PranchaComunicacaoApplication {
      * Inicializa o diretório de armazenamento de arquivos - Roda automático
      */
     @Bean
-    CommandLineRunner init(ArmazenamentoDiscoServico armazenamento) {
+    CommandLineRunner init(ArmazenamentoDiscoServico armazenamento, SujeitoServico ss,
+                           VerboServico vs, PacienteServico ps, ComplementoServico cs,
+                           DiversoServico ds) {
         return (args) -> {
+    
+            for (Paciente p : ps.buscarTodos()) {
+                p.setSujeitos(new HashSet<>(ss.buscarTodos()));
+                p.setVerbos(new HashSet<>(vs.buscarTodos()));
+                p.setComplementos(new HashSet<>(cs.buscarTodos()));
+                p.setDiversos(new HashSet<>(ds.buscarTodos()));
+        
+                ps.salvar(p);
+            }
+            //TODO: Remover entradas de teste acima
+            
             armazenamento.init();
         };
     }
