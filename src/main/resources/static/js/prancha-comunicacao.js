e82fb9bc8ebb6e9c560ef8222a7b92e4124676eb
CleytonGoncalves <cleyton.cgoncalves@gmail.com>
2018-03-10 19:04:43 -0400

    Adiciona seleção de itens

--- src/main/resources/static/js/prancha-comunicacao.js
+++ src/main/resources/static/js/prancha-comunicacao.js
@@ -7,13 +7,18 @@ var TEMPO_SELETOR = 1000; // Tempo em ms para o movimento do seletor
 
 /* ### CONSTANTES DO SCRIPT ### */
 
-/* Classes CSS */
+/* CSS */
 
 var CLASSE_SECAO_SELECIONAVEL = "secao-selecionavel";
 var CLASSE_ITEM_SELECIONAVEL = "item-selecionavel";
 
-var CLASSE_SECAO_SELECIONADA = "secao-realce";
-var CLASSE_ITEM_SELECIONADO = "item-realce";
+var CLASSE_SECAO_REALCE = "secao-realce";
+var CLASSE_ITEM_REALCE = "item-realce";
+
+var CLASSE_ITEM_ACAO = "item-acao";
+var CLASSE_ITEM_SELECIONADO = "item-selecionado";
+
+var ID_SECAO_FORMACAO = "secao-formacao";
 
 
 /* Enum de estados do seletor */
@@ -25,8 +30,8 @@ var EstadoSeletorEnum = {
 };
 
 if (Object.freeze) {
-    Object.freeze(EstadoSeletorEnum);
-} // Torna imutável se suportar ES5
+    Object.freeze(EstadoSeletorEnum); // Torna imutável se suportar ES5
+}
 
 
 /* ### SCRIPT DO SELETOR ### */
@@ -34,25 +39,37 @@ if (Object.freeze) {
 var estadoSeletor = EstadoSeletorEnum.SELETOR_PARADO;
 var seletorIntervalId = undefined;
 
-$(window).ready(function () {
-    rodarSeletorSecao();
-});
+$(window).ready(rodarSeletorSecao);
 
-$("body").not("#topo").click(clickFeito); // Qualquer parte da página, exceto pelo navbar
 
 /* Clique */
+
+$("body").not("#topo").click(clickFeito); // Qualquer parte da página, exceto pelo navbar
 
 function clickFeito() {
     if (estadoSeletor === EstadoSeletorEnum.SELETOR_SECAO) {
         rodarSeletorItem();
     } else if (estadoSeletor === EstadoSeletorEnum.SELETOR_ITEM) {
-        //    TODO: MOVER O ITEM SELECIONADO PARA A SECAO DE EXIBIÇÃO, RETIRAR DA SECAO ANTERIOR, DESATIVAR REALCE
         pararSeletor();
+
+        var itemRealcado = getItemRealcado();
+        if (itemRealcado.hasClass(CLASSE_ITEM_ACAO)) {
+            //fazerAcao(itemRealcado);
+        } else {
+            selecionarItem(itemRealcado);
+        }
+
+        desativarTodosRealces();
+        rodarSeletorSecao();
     }
 }
 
+/* Seletor */
+
 function rodarSeletorSecao() {
-    pararSeletor();
+    if (estadoSeletor !== EstadoSeletorEnum.SELETOR_PARADO) {
+        pararSeletor();
+    }
 
     var secoes = getSecoesSelecionaveis();
     estadoSeletor = EstadoSeletorEnum.SELETOR_SECAO;
@@ -60,14 +77,14 @@ function rodarSeletorSecao() {
 }
 
 function rodarSeletorItem() {
-    pararSeletor();
+    if (estadoSeletor !== EstadoSeletorEnum.SELETOR_PARADO) {
+        pararSeletor();
+    }
 
     var itensSecaoEscolhida = getItensSelecionaveis();
     estadoSeletor = EstadoSeletorEnum.SELETOR_ITEM;
     iniciarSeletor(itensSecaoEscolhida, ativarRealceItem, desativarRealceItem);
 }
-
-/* Seletor */
 
 function iniciarSeletor(lista, funcaoAtivarRealce, funcaoDesativarRealce) {
     var posAtual = 0;
@@ -88,7 +105,7 @@ function pararSeletor() {
 
     if (seletorIntervalId) {
         clearInterval(seletorIntervalId);
-        seletorIntervalId = undefined;
+        seletorIntervalId = null;
     }
 }
 
@@ -100,28 +117,59 @@ function calcPosCircular(pos, qntdSecao) {
 /* Realce */
 
 function ativarRealceSecao(secao) {
-    secao.classList.add(CLASSE_SECAO_SELECIONADA);
+    secao.classList.add(CLASSE_SECAO_REALCE);
 }
 
 function desativarRealceSecao(secao) {
-    secao.classList.remove(CLASSE_SECAO_SELECIONADA);
+    secao.classList.remove(CLASSE_SECAO_REALCE);
 }
 
 function ativarRealceItem(item) {
-    item.classList.add(CLASSE_ITEM_SELECIONADO);
+    item.classList.add(CLASSE_ITEM_REALCE);
 }
 
 function desativarRealceItem(item) {
-    item.classList.remove(CLASSE_ITEM_SELECIONADO);
+    item.classList.remove(CLASSE_ITEM_REALCE);
+}
+
+function desativarTodosRealces() {
+    $("." + CLASSE_SECAO_REALCE).removeClass(CLASSE_SECAO_REALCE);
+    $("." + CLASSE_ITEM_REALCE).removeClass(CLASSE_ITEM_REALCE);
 }
 
 /* Funções Ajudantes */
 
 function getSecoesSelecionaveis() {
-    return $("." + CLASSE_SECAO_SELECIONAVEL + ":visible");
+    return $("." + CLASSE_SECAO_SELECIONAVEL).filter(":visible");
 }
 
 function getItensSelecionaveis() {
-    var selector = "." + CLASSE_SECAO_SELECIONADA + " ." + CLASSE_ITEM_SELECIONAVEL + ":visible";
-    return $(selector);
+    var selector = "." + CLASSE_SECAO_REALCE + " ." + CLASSE_ITEM_SELECIONAVEL;
+    return $(selector).filter(":visible");
 }
+
+function getItemRealcado() {
+    return $("." + CLASSE_SECAO_REALCE + " ." + CLASSE_ITEM_REALCE).filter(":visible");
+}
+
+function selecionarItem(item) {
+    item.detach();
+
+    // Deixa de ser selecionavel
+    item.removeClass(CLASSE_ITEM_SELECIONAVEL);
+    item.addClass(CLASSE_ITEM_SELECIONADO);
+
+    moverParaSecaoFormacao(item);
+}
+
+function moverParaSecaoFormacao(item) {
+    item.appendTo("#" + ID_SECAO_FORMACAO).scrollView();
+}
+
+$.fn.scrollView = function () {
+    return this.each(function () {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top - 20
+        }, 500);
+    });
+};
