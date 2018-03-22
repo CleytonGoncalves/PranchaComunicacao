a8e94f2c6d43309503c987096384b2232a5213ff
Cleyton <cleyton@cleytongoncalves.com>
2018-03-22 19:12:27 -0400

    Corrige log para utilizar String Format

--- src/main/java/br/ufmt/ic/fata/PranchaComunicacao/servico/palavra/PalavraServico.java
+++ src/main/java/br/ufmt/ic/fata/PranchaComunicacao/servico/palavra/PalavraServico.java
@@ -1,14 +1,13 @@
 package br.ufmt.ic.fata.PranchaComunicacao.servico.palavra;
 
-import org.springframework.validation.Errors;
-import org.springframework.web.multipart.MultipartFile;
-
 import br.ufmt.ic.fata.PranchaComunicacao.modelo.Palavra;
 import br.ufmt.ic.fata.PranchaComunicacao.repositorio.PalavraRepositorio;
 import br.ufmt.ic.fata.PranchaComunicacao.servico.ValidadorImagem;
 import br.ufmt.ic.fata.PranchaComunicacao.servico.armazenamento.ArmazenamentoServico;
 import br.ufmt.ic.fata.PranchaComunicacao.servico.base.ServicoCrudBase;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.Errors;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * <a href=https://stackoverflow.com/q/48125594/9180376>Por que não é possível fazer um único
@@ -53,11 +52,11 @@ public abstract class PalavraServico<T extends Palavra> extends ServicoCrudBase<
     
     private void logPalavraSalva(T palavra) {
         log.debug("\n--- Palavra Salva ---\n" +
-                          "Palavra: " + palavra.getPalavra() + "\n" +
-                          "Novo: " + palavra.isNew() + "\n" +
-                          "ID: " + palavra.getId() + "\n" +
-                          "Imagem: " + palavra.getUrl() + "\n" +
-                          "--- ---\n");
+                "Palavra: {}\n" +
+                "Novo: {}\n" +
+                "ID: {}\n" +
+                "Imagem: {}\n" +
+                "--- ---\n", palavra.getPalavra(), palavra.isNew(), palavra.getId(), palavra.getUrl());
     }
     
 }
