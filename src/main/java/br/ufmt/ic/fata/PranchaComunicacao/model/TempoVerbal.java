b2d06a227e2d99ea1bd059150b4a3181dc91a66d
Cleyton <cleyton@cleytongoncalves.com>
2018-06-24 17:31:11 -0400

    Adiciona anotações p/ Json do Tempo Verbal

--- src/main/java/br/ufmt/ic/fata/PranchaComunicacao/model/TempoVerbal.java
+++ src/main/java/br/ufmt/ic/fata/PranchaComunicacao/model/TempoVerbal.java
@@ -1,5 +1,8 @@
 package br.ufmt.ic.fata.PranchaComunicacao.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TempoVerbal {
     PASSADO("Passado"), PRESENTE("Presente"), FUTURO("Futuro");
     
@@ -9,6 +12,7 @@ public enum TempoVerbal {
         this.value = value;
     }
     
+    @JsonCreator
     public static TempoVerbal fromValue(String value) {
         for (TempoVerbal tempo : values()) {
             if (tempo.value.equalsIgnoreCase(value)) {
@@ -16,6 +20,11 @@ public enum TempoVerbal {
             }
         }
         throw new IllegalArgumentException("Valor de enum desconhecido: " + value + ".");
+    }
+    
+    @JsonValue
+    public String toValue() {
+        return this.toString();
     }
     
 }
