a6bbf03eb3381e02e97a318eb086bf488bc17683
Cleyton <cleyton@cleytongoncalves.com>
2018-06-26 22:37:53 -0400

    Delega gerenciamento de fluxo para o objeto FluxoPrancha

--- src/main/resources/static/js/prancha/prancha-comunicacao.js
+++ src/main/resources/static/js/prancha/prancha-comunicacao.js
@@ -1,60 +1,8 @@
 "use strict";
 
-//TODO: Relacionar Tempo com o Verbo, e ao remover o Tempo, voltar p/ tela de seleção do Tempo
+//TODO: Fazer com que ao APAGAR ÚLTIMO  após Tempo Verbal, retorne a seleção de tempo
 //TODO: Remover uso de IDs/Classes em favor do uso de Data-Attributes no JS e CSS
 //TODO: https://www.w3.org/wiki/JavaScript_best_practices#Avoid_globals
-//TODO: Criar objeto global que guarda o estado do app
-
-/* ##### CONSTANTES DO SCRIPT ##### */
-
-/* CSS */
-
-/* Classes que definem quais são selecionáveis pelo seletor */
-var CLASSE_SECAO_SELECIONAVEL = "secao-selecionavel";
-
-/* Classes que aplicam o realce (seleção) */
-var CLASSE_SECAO_REALCE = "secao-realce";
-var CLASSE_ITEM_REALCE = "item-realce";
-
-/* Classe que define os itens que foram selecionados*/
-var CLASSE_ITEM_SELECIONADO = "item-selecionado";
-
-/* Classe que define o texto dos itens */
-var CLASSE_ITEM_TEXTO = "texto-item";
-
-/* IDs das seções, para usar na montagem do fluxo */
-var ID_SECAO_FORMACAO = "secao-formacao";
-var ID_SECAO_SUJEITO = "secao-sujeito";
-var ID_SECAO_VERBO = "secao-verbo";
-var ID_SECAO_COMPLEMENTO = "secao-complemento";
-var ID_SECAO_DIVERSO = "secao-diverso";
-var ID_SECAO_ACAO = "secao-acao";
-var ID_SECAO_TEMPO = "secao-tempo";
-
-/* ### Atributos Data-* personalizados ### */
-
-/* Chaves */
-var ATR_ITEM_TIPO = "itemTipo";
-var ATR_ITEM_SECAO = "itemSecao";
-
-var ATR_PALAVRA_CATEGORIA = "palavraCategoria";
-var ATR_PALAVRA_ID = "palavraId";
-
-var ATR_ACAO_ID = "acaoId";
-
-var ATR_TEMPO_ID = "tempoId";
-
-/* Valores */
-var VAL_TIPO_PALAVRA = "palavra";
-var VAL_TIPO_ACAO = "acao";
-var VAL_TIPO_TEMPO = "tempo";
-
-var VAL_PALAVRA_CATEGORIA_VERBO = "verbo";
-
-var VAL_ACAO_FALAR = "falar";
-var VAL_ACAO_APAGAR_ULTIMO = "apagar-ultimo";
-var VAL_ACAO_VOLTAR_SELECAO = "voltar-selecao";
-var VAL_ACAO_RECOMECAR = "recomecar";
 
 /* ### Enum de estados do seletor ### */
 
@@ -68,7 +16,7 @@ var EstadoSeletorEnum = {
 /* ##### INICIALIZAÇÃO DA PRANCHA ##### */
 
 var sintetizador = new Sintetizador();
-var fluxo = new FluxoPrancha();
+var fluxo = new FluxoPrancha(CONFIG_FLUXO_PRANCHA);
 
 var listaPalavrasSelecionadas = [];
 var tempoVerbalSelecionado = CONFIG_TEMPO_VERBAL_PADRAO;
@@ -76,25 +24,21 @@ var tempoVerbalSelecionado = CONFIG_TEMPO_VERBAL_PADRAO;
 var estadoSeletor = EstadoSeletorEnum.SELETOR_PARADO;
 var seletorIntervalId = undefined;
 
-var fluxoFuncoes = null;
-var etapaFluxoAtual = null;
-
-
 /* ##### SCRIPT DO SELETOR ##### */
 
 $(window).ready(function () {
-    fluxoFuncoes = fluxo.getFluxoPaciente();
-    proximaEtapaFluxo(null);
+    proximaEtapaFluxo();
 });
 
 /* ### Seletor ### */
 
 function rodarSeletorSecao() {
     pararSeletor();
+    desativarTodosRealces();
     
     var secoes = getSecoesVisiveisSelecionaveis();
     estadoSeletor = EstadoSeletorEnum.SELETOR_SECAO;
-    iniciarSeletor(secoes, ativarRealceSecao, desativarRealceSecao);
+    rodarSeletor(secoes, ativarRealceSecao, desativarRealceSecao);
 }
 
 function rodarSeletorItem() {
@@ -102,10 +46,10 @@ function rodarSeletorItem() {
     
     var itensSecaoEscolhida = getItensSelecionaveis();
     estadoSeletor = EstadoSeletorEnum.SELETOR_ITEM;
-    iniciarSeletor(itensSecaoEscolhida, ativarRealceItem, desativarRealceItem);
+    rodarSeletor(itensSecaoEscolhida, ativarRealceItem, desativarRealceItem);
 }
 
-function iniciarSeletor(lista, funcaoAtivarRealce, funcaoDesativarRealce) {
+function rodarSeletor(lista, funcaoAtivarRealce, funcaoDesativarRealce) {
     var posAtual = 0;
     var qntd = lista.length;
     
@@ -136,11 +80,6 @@ function pararSeletor() {
     }
 }
 
-function calcPosCircular(pos, qntdSecao) {
-    // Posicao circular: pos -1 -> qntdSecao-1; pos 0 -> 0; ...; pos qntdSecao -> 0; pos qntdSecao + 1 -> 1;
-    return (pos + qntdSecao) % qntdSecao; //
-}
-
 /* ### Ao Clicar ### */
 
 $("body").not("#topo").click(selecionarAtual); // Qualquer parte da página, exceto pelo navbar
@@ -162,24 +101,19 @@ function selecionarAtual() {
 }
 
 function selecionarItem(item) {
-    item.detach();
-    
-    // Deixa de ser selecionável, e se torna selecionado
-    item.addClass(CLASSE_ITEM_SELECIONADO);
-    
     var tipoItem = item.data(ATR_ITEM_TIPO);
     
-    switch (tipoItem) {
-        case VAL_TIPO_TEMPO:
-            tempoVerbalSelecionado = item.data(ATR_TEMPO_ID);
-            // Clone permite escolher o mesmo Tempo p/ verbos diferentes do mesmo fluxo, caso liberado
-            item = item.clone(true, true);
-            break;
-        case VAL_TIPO_PALAVRA:
-            listaPalavrasSelecionadas.push(item.data(ATR_PALAVRA_ID));
-            break;
-        default:
-            console.error("Seleção de item inválido: {0}".f(item));
+    if (tipoItem === VAL_TIPO_TEMPO) {
+        tempoVerbalSelecionado = item.data(ATR_TEMPO_ID);
+        // Clone permite escolher o mesmo Tempo p/ verbos diferentes do mesmo fluxo, caso liberado
+        item = item.clone(true, true);
+    }
+    
+    item.detach();
+    item.addClass(CLASSE_ITEM_SELECIONADO);
+    
+    if (tipoItem === VAL_TIPO_PALAVRA) {
+        listaPalavrasSelecionadas.push(item.data(ATR_PALAVRA_ID));
     }
     
     moverParaSecaoFormacao(item);
@@ -187,26 +121,24 @@ function selecionarItem(item) {
 
 function deselecionarItem(item) {
     item.detach();
-    
-    // Deixa de ser selecionado, e se torna selecionável
     item.removeClass(CLASSE_ITEM_SELECIONADO);
     
     var tipoItem = item.data(ATR_ITEM_TIPO);
     
-    switch (tipoItem) {
-        case VAL_TIPO_TEMPO:
-            tempoVerbalSelecionado = CONFIG_TEMPO_VERBAL_PADRAO;
-            break;
-        case VAL_TIPO_PALAVRA:
-            var index = listaPalavrasSelecionadas.indexOf(item.data(ATR_PALAVRA_ID));
-            if (index > -1)
-                listaPalavrasSelecionadas = listaPalavrasSelecionadas.splice(index, 1); // Remove o ID da palavra atual da lista
-            break;
-        default:
-            console.error("Deseleção de item inválido: {0}".f(item));
+    if (tipoItem === VAL_TIPO_TEMPO) {
+        tempoVerbalSelecionado = CONFIG_TEMPO_VERBAL_PADRAO;
+        return;
     }
     
-    moverParaSecaoOriginal(item);
+    if (tipoItem === VAL_TIPO_PALAVRA) {
+        listaPalavrasSelecionadas = listaPalavrasSelecionadas.filter(function (e) {
+            return e !== item.data(ATR_PALAVRA_ID);
+        }); // Remove o ID da palavra atual da lista
+        moverParaSecaoOriginal(item);
+        return;
+    }
+    
+    console.error("Deseleção de item inválido: {0}".f(item));
 }
 
 function fazerAcao(item) {
@@ -215,17 +147,16 @@ function fazerAcao(item) {
     switch (tipoAcao) {
         case VAL_ACAO_FALAR:
             fazerAcaoFalar();
+            rodarSeletorSecao();
             break;
         case VAL_ACAO_VOLTAR_SELECAO:
-            desativarTodosRealces();
             rodarSeletorSecao();
             break;
         case VAL_ACAO_RECOMECAR:
             window.location.reload(true);
             break;
         case VAL_ACAO_APAGAR_ULTIMO:
-            deselecionarItem($("#{0}".f(ID_SECAO_FORMACAO)).scrollView().children().last());
-            voltarEtapaFluxo();
+            voltarEtapaFluxo(item);
             break;
         default:
             console.error("Ação da prancha inválida: {0}".f(tipoAcao));
@@ -255,38 +186,13 @@ function fazerAcaoFalar() {
 /* ### Fluxo ### */
 
 function proximaEtapaFluxo(ultimoItemSelecionado) {
-    desativarTodosRealces();
-    
-    if (etapaFluxoAtual === null || etapaFluxoAtual < 0) {
-        etapaFluxoAtual = 0;
-    } else if (etapaFluxoAtual < fluxoFuncoes.length - 1) {
-        etapaFluxoAtual++;
-    } else {
-        rodarSeletorSecao();
-        return; // Mantém preso na última etapa, podendo selecionar quantos itens/ações quiser
-    }
-    
-    var funcaoAtualFluxo = fluxoFuncoes[etapaFluxoAtual];
-    funcaoAtualFluxo(ultimoItemSelecionado);
-    
+    fluxo.avancar(ultimoItemSelecionado);
     rodarSeletorSecao();
 }
 
-function voltarEtapaFluxo() {
-    desativarTodosRealces();
-    
-    if (etapaFluxoAtual === null || etapaFluxoAtual < 0) {
-        etapaFluxoAtual = 0;
-    } else if (etapaFluxoAtual >= 1) {
-        etapaFluxoAtual--;
-    } else {
-        rodarSeletorSecao();
-        return; // Mantém preso na primeira etapa
-    }
-    
-    var funcaoAtualFluxo = fluxoFuncoes[etapaFluxoAtual];
-    funcaoAtualFluxo();
-    
+function voltarEtapaFluxo(ultimoItemSelecionado) {
+    deselecionarItem($("#{0}".f(ID_SECAO_FORMACAO)).scrollView().children().last());
+    fluxo.voltar(ultimoItemSelecionado);
     rodarSeletorSecao();
 }
 
@@ -294,6 +200,11 @@ function voltarEtapaFluxo() {
 /* ##### FUNÇÕES AJUDANTES ##### */
 
 /* ### Seletor ### */
+
+function calcPosCircular(pos, qntdSecao) {
+    // Posicao circular: pos -1 -> qntdSecao-1; pos 0 -> 0; ...; pos qntdSecao -> 0; pos qntdSecao + 1 -> 1;
+    return (pos + qntdSecao) % qntdSecao; //
+}
 
 function getSecoesVisiveisSelecionaveis() {
     return $("." + CLASSE_SECAO_SELECIONAVEL).filter(":visible");
@@ -312,6 +223,7 @@ function moverParaSecaoFormacao(item) {
 }
 
 function moverParaSecaoOriginal(item) {
+    // TODO: Voltar o item para mesma posição anterior, ou ao menos coloca-lo antes do Voltar Seleção
     item.appendTo("#{0} ul".f(item.data(ATR_ITEM_SECAO)));
 }
 
