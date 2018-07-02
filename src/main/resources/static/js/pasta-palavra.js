350ee7ecf100dd156fc71d9f4a98454281712b3b
Cleyton <cleyton@cleytongoncalves.com>
2018-07-01 22:42:51 -0400

    Adiciona DataTables e cabeçalho nas páginas de palavras

--- src/main/resources/static/js/pasta-palavra.js
+++ src/main/resources/static/js/pasta-palavra.js
@@ -1,12 +1,11 @@
 "use strict";
 
-var TAMANHO_MAX_IMAGEM = 2000; // Comprimento/Largura em px
-var TAMANHO_MIN_IMAGEM = 100; // Comprimento/Largura em px
-var PESO_MAX_IMAGEM = 2000; // Peso em KB
-
 $(document).ready(function () {
     $("#botao-novo-cadastro").click(clickNovoCadastro);
-    aplicarTabelaListeners();
+    
+    var dataTablesConfig = jQuery.extend({ "initComplete": aplicarTabelaListeners }, DATATABLES_CONFIG_PADRAO);
+    
+    $("#tabela-palavras").DataTable(dataTablesConfig);
 });
 
 /* Pede pro servidor criar uma nova palavra, recebendo o modal  */
@@ -46,52 +45,6 @@ function adicionarFormulario(novoFormHtml) {
     $('#modal-cadastro').modal('show'); // Exibe o modal
 
     aplicarModalListeners();
-}
-
-function validarImagem(pesoKB) {
-    var altura = this.height;
-    var largura = this.width;
-
-    if (altura < TAMANHO_MIN_IMAGEM || largura < TAMANHO_MIN_IMAGEM) {
-        alert("A imagem é muito pequena. As dimensões devem estar entre "
-            + TAMANHO_MIN_IMAGEM + "px por " + TAMANHO_MIN_IMAGEM + "px.");
-        return false;
-    }
-
-    if (altura > TAMANHO_MAX_IMAGEM || largura > TAMANHO_MAX_IMAGEM) {
-        alert("A imagem é muito grande. As dimensões devem estar entre "
-            + TAMANHO_MAX_IMAGEM + "px por " + TAMANHO_MAX_IMAGEM + "px.");
-        return false;
-    }
-
-    if (pesoKB > PESO_MAX_IMAGEM) {
-        alert("A imagem é muito pesada. Peso máximo permitido é de " +
-            PESO_MAX_IMAGEM + "KB.");
-        return false;
-    }
-
-    return true;
-}
-
-/* Pré-Visualização da imagem escolhida no formulário */
-function carregarImagemPreview(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        var arquivo = input.files[0];
-
-        reader.onload = function (e) {
-            var imagemTag = $('#imagem-palavra');
-            imagemTag.attr("src", e.target.result);
-
-            //TODO: Ativar validação de tamanho de imagem
-            //var pesoKB = Math.round(arquivo.size / 1024);
-            //imagemTag.onload = function() {
-            //  validarImagem(pesoKB);
-            //}
-        };
-
-        reader.readAsDataURL(arquivo);
-    }
 }
 
 /* Envia o formulário pro servidor, recebendo a tabela de palavras atualizada */
@@ -134,16 +87,6 @@ function aplicarTabelaListeners() {
 }
 
 function aplicarModalListeners() {
-    /* Listener para habilitar pré-visualização ao selecionar imagem */
-    $("#input-imagem-real").change(function () {
-        carregarImagemPreview(this);
-    });
-
-    /* Repassa o click no botão falso de seleção de imagem para o botão real */
-    $("#input-imagem-exibido").click(function () {
-        $('#input-imagem-real').click()
-    });
-
     /* Listener para fazer o envio do formulário */
     $("#form-cadastro").submit(onEnviarForm);
 }
