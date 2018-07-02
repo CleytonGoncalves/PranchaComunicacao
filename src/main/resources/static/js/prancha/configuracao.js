10d3908fe09dced64a740dacc135ac7890c8fc56
Cleyton <cleyton@cleytongoncalves.com>
2018-07-01 22:37:05 -0400

    Move configuração individual da prancha p/ html com thymeleaf
    Possibilitando o uso do Model

--- src/main/resources/static/js/prancha/configuracao.js
+++ src/main/resources/static/js/prancha/configuracao.js
@@ -1,8 +1,4 @@
-/* ##### CONFIGURAÇÕES DA PRANCHA DE ACORDO COM O PACIENTE ##### */
+/* ##### CONFIGURAÇÃO GERAL DA PRANCHA ##### */
 
-//TODO: Disponibilizar as configurações da prancha como opção na criação do paciente
-
-var CONFIG_TEMPO_SELETOR = 1000; // Tempo em ms para o movimento do seletor
-var CONFIG_FALAR_CADA_PALAVRA = false; // Se todo item pelo qual o seletor passar deve ser falado
-var CONFIG_FLUXO_PRANCHA = "FLUXOLIVRE"; // Qual fluxo de prancha usar
 var CONFIG_TEMPO_VERBAL_PADRAO = "Presente"; // Tempo verbal padrão
+
